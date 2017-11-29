d5a3c69f224fae836e0f436673c644fd5e8c6453
binabdul <m.ainolfhatah@gmail.com>
2017-11-29 10:37:01 +0100

    UDP Ok

--- src/main/java/com/fredericboisguerin/insa/network/core/service/UDPMessageReceiverService.java
+++ src/main/java/com/fredericboisguerin/insa/network/core/service/UDPMessageReceiverService.java
@@ -8,8 +8,9 @@ public class UDPMessageReceiverService implements MessageReceiverService {
     @Override
     public void listenOnPort(int port, IncomingMessageListener incomingMessageListener) throws Exception {
         DatagramSocket receiverSocket = new DatagramSocket(port);
-        DatagramPacket receivedPacket = new DatagramPacket(new byte[1500], 1500);
+        DatagramPacket receivedPacket = new DatagramPacket(new byte[500], 500);
         receiverSocket.receive(receivedPacket);
         byte[] data = receivedPacket.getData();
+        incomingMessageListener.onNewIncomingMessage(new String(data));
     }
 }
